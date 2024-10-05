0db46dc324d21caf4ebc7b863768b78317d829d3
Amin A Talahmeh <ayman201266@hotmail.com>
2024-10-05 03:47:22 +0300

    update db config

--- db/connect.php
+++ db/connect.php
@@ -1,15 +1,20 @@
 <?php
-// Database connection
-$db_host = 'localhost';
-$db_username = 'root';
-$db_password = '';
-$db_name = 'academy';
 
-$conn = mysqli_connect($db_host, $db_username, $db_password, $db_name);
+# Load db configs
+$env = parse_ini_file("../.env");
+
+// Database connection
+$db_host = isset($env['DB_HOST']) ? $env['DB_HOST'] : 'localhost';
+$db_user = isset($env['DB_USER']) ? $env['DB_USER'] : 'root';
+$db_pass = isset($env['DB_PASS']) ? $env['DB_PASS'] : '';
+$db_name = isset($env['DB_NAME']) ? $env['DB_NAME'] : 'academy';
+
+// Proceed with connecting to the database
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
 // Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+if (!$conn || $conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
 
